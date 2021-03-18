0f8c1557ca163e138ade8d5bb659d924414399bb
Pablo <pablo.alberto.zabala@gmail.com>
2021-03-17 21:20:37 -0300

    Se han limpiado los getters del store almacen

--- src/store/almacen/getters.ts
+++ src/store/almacen/getters.ts
@@ -1,57 +1,7 @@
 import { GetterTree } from "vuex"
-import { AlmacenState } from "@/store/almacen/types"
-import { RootState } from '@/store/types'
+import { AlmacenState } from "./types"
+import { RootState } from '../types'
 
 export const getters: GetterTree<AlmacenState, RootState> = {
-  getData(state): Array<object> {
-    return state.data
-  },
-  ocupado(state): boolean {
-    return state.ocupado;
-  },
-  getProcedimientoOrden(state) {
-    return (payload: any) => {
-      const index = payload.index;
-      const ordenId = payload.ordenId;
-      if (state.data[index].orden.id == ordenId) {
-        return state.data[index].procedimiento;
-      } else return undefined;
-    }
-  },
-  getFechaOrden(state) {
-    return (payload: any) => {
-      const index = payload.index;
-      const ordenId = payload.ordenId;
-      if (state.data[index].orden.id == ordenId) {
-        return state.data[index].orden.fecha;
-      } else return undefined;
-    }
-  },
-  getConvenio(state) {
-    return (payload: any) => {
-      const index = payload.index;
-      const ordenId = payload.ordenId;
-      if (state.data[index].orden.id == ordenId) {
-        return state.data[index].datos_convenio;
-      } else return undefined;
-    }
-  },
-  getViaFacturacion(state) {
-    return (payload: any) => {
-      const index = payload.index;
-      const ordenId = payload.ordenId;
-      if (state.data[index].orden.id == ordenId) {
-        return state.data[index].datos_convenio.convenio.via;
-      } else return undefined;
-    }
-  },
-  getFinanciador(state) {
-    return (payload: any) => {
-      const index = payload.index;
-      const ordenId = payload.ordenId;
-      if (state.data[index].orden.id == ordenId) {
-        return state.data[index].financiador;
-      } else return undefined;
-    }
-  }
+  
 }
